6e8669e5a539e52e24edd4f33deb79d814e47dde
JoshuaMcGill <joshua.mcgill092@escapestudios.ac.uk>
2025-10-13 13:30:53 +0100

    More progress on inputs

--- Unreal Project/Annotation_Tool/Content/Python/AnnotationPythonScript.py
+++ Unreal Project/Annotation_Tool/Content/Python/AnnotationPythonScript.py
@@ -1,13 +1,20 @@
 import unreal
+from pynput.keyboard import Key, Listener
+import keyboard
+from collections import deque
+from collections.abc import Iterable
 import sys
+from PySide6 import QtCore
 from unreal import SystemLibrary
 from functools import partial
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QSlider,QVBoxLayout, QLabel, QCheckBox, QComboBox, QGridLayout, QBoxLayout, QGridLayout, QDialog
-
-
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QSlider, QVBoxLayout, QLabel, QCheckBox, QComboBox, QGridLayout, QBoxLayout, QGridLayout, QDialog
+import time
+ 
+ 
 #TO DO:
-#make colour selection menu
+#make colour selection menu (returning to this later)
+#make a "start drawing" button on the UI that opens a transparent window that covers the whole screen. this way it can detect key inputs AND stop me moving about the camera using the mouse. Esc to close it
 #make the tool (good luck)
 coloursArray = (
         "black",
@@ -20,14 +27,16 @@ coloursArray = (
         "blue",
         "cyan",
         "magenta"
-
+ 
 )
-
+ 
 selectedColour = "background-color : green"
 
-
-
-
+toolRunning = True
+EAS = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
+ 
+ 
+ 
 class UnrealToolWindow(QWidget):
     def __init__ (self, parent = None):
        
@@ -41,7 +50,7 @@ class UnrealToolWindow(QWidget):
         # Create a Click Event that when the button is clicked it will call the function
         # ButtonClicked()
         currentColour = selectedColour
-
+ 
  
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setMinimum(0)
@@ -52,20 +61,24 @@ class UnrealToolWindow(QWidget):
         self.sliderLabel = QLabel()
         self.sliderLabel.setText("5")
         #self.sliderLabel.setAlignment(Qt.AlignJustify)
-
+ 
         self.sliderName = QLabel("Line Width:")
-
+ 
         self.modeLabel = QLabel("Mode:")
  
         self.dropDown = QComboBox()
-
+ 
         self.colourPickerButton = QPushButton()
         self.colourPickerButton.setMaximumWidth(30)
         self.colourPickerButton.setStyleSheet("background-color : cyan")
-
+ 
         self.colourPickerLabel = QLabel("Line Colour:")
-
+ 
         self.colourPickerButton.clicked.connect(self.colourPickerButtonClicked)
+
+        self.drawButton = QPushButton("Start Drawing")
+        self.drawButton.setMaximumWidth(200)
+        self.drawButton.clicked.connect(self.drawButtonClicked)
  
         ##################################
  
@@ -79,6 +92,7 @@ class UnrealToolWindow(QWidget):
         layout.addWidget(self.slider, 1, 2)
         layout.addWidget(self.colourPickerLabel, 2, 0)
         layout.addWidget(self.colourPickerButton, 2, 2)
+        layout.addWidget(self.drawButton, 3, 2)
  
         container = QWidget()
         container.setLayout(layout)
@@ -87,16 +101,16 @@ class UnrealToolWindow(QWidget):
         self.main_window.setMenuWidget(container)
         self.dropDown.addItem("Screen Space")
         self.dropDown.addItem("Surface Snapping")
-
+ 
     def updateButtonColour(self, selectedColour):
         self.colourPickerButton.setStyleSheet("background-color : red")
         unreal.log(selectedColour)
-        
+       
  
     def sliderChanged(self,value):
         unreal.log("Slider was moved to: " + str(value))
         self.sliderLabel.setText(str(value))
-    
+   
     def colourPickerButtonClicked(self):
         unreal.log("BUTTON CLICKED")
         self.colourWindow = ColourWindow()
@@ -105,15 +119,25 @@ class UnrealToolWindow(QWidget):
         self.colourPickerButton.setStyleSheet(f"{selectedColour}")
         fish = self.colourPickerButton.styleSheet()
         print (fish)
-    
+
+    def drawButtonClicked(self):
+        unreal.log("Started Drawing")
+        self.transparentWindow = TransparentWindow()
+        self.transparentWindow.show()
+
+    # def keyPressEvent(self, event):
+    #     print(event.key())
+    #     # if event.key() == Qt.Key_Space:
+    #     #     print("Hello")
+   
 class ColourWindow(QWidget):
     def __init__(self, parent = UnrealToolWindow):
         super().__init__()
         colourLayout = QGridLayout()
-
+ 
         gridX = 0
         gridY = 0
-
+ 
         self.setLayout(colourLayout)
         self.colour_window = QMainWindow()
         #self.colour_window.setParent(self)
@@ -132,15 +156,24 @@ class ColourWindow(QWidget):
                 gridX = gridX + 1
                 gridY = 0
             self.colourButton.clicked.connect(self.ColourButtonClicked)
-        
+       
     def ColourButtonClicked(self):
         colour = self.sender().styleSheet()
         buttonColour = colour
         selectedColour = self.sender().styleSheet()
         mainWindow = UnrealToolWindow()
         mainWindow.updateButtonColour(selectedColour)
+   
+class TransparentWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.transparent_window = QMainWindow()
+        self.transparent_window.setFixedSize(QSize(3000, 3000))
+        self.transparent_window.setMinimumSize(QSize(3000, 3000))
     
-            
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            print("Hello you fool")
  
 def launchWindow():
     if QApplication.instance():
@@ -155,22 +188,141 @@ def launchWindow():
     UnrealToolWindow.window.setWindowTitle("Annotation Tool")
     UnrealToolWindow.window.show()
     unreal.parent_external_window_to_slate(UnrealToolWindow.window.winId())
-
+ 
 launchWindow()
-
+ 
 class SplineActor():
     EAS = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
     # viewport = unreal.Viewport()
     # camLocation = unreal.Viewport.get_view_rotation(viewport)
-    cameraManager = unreal.PlayerCameraManager()
-    camLocation = unreal.EditorLevelLibrary.get_level_viewport_camera_info()
-    unreal.log(camLocation)
-    # actorClass = unreal.StaticMeshActor
-    # componentClass = unreal.StaticMeshComponent
-    # location = unreal.Vector(0, 0, 0)
-    # staticMeshActor = EAS.spawn_actor_from_class(actorClass, camLocation)
-    # staticMesh = unreal.EditorAssetLibrary.load_asset('/Engine/BasicShapes/Cube.Cube')
+    # camLocation = unreal.EditorLevelLibrary.get_level_viewport_camera_info()
+    UES = unreal.UnrealEditorSubsystem()
+    camLocation = unreal.UnrealEditorSubsystem.get_level_viewport_camera_info(UES)
+    # print (camLocation)
+    cameraValues = []
+   
+    for x in camLocation:
+        # print(type(x))
+        cameraValues.append(x)
+ 
+    print(cameraValues[0])
+    # unreal.log(camLocation)
+ 
+    mouse = unreal.MouseInputDeviceState()
+    RMB = mouse.right.__getattribute__("down")
+    print(RMB)
+ 
+    actorClass = unreal.StaticMeshActor
+    componentClass = unreal.StaticMeshComponent
+    location = unreal.Vector(0, 0, 200)
+    staticMeshActor = EAS.spawn_actor_from_class(actorClass, cameraValues[0])
+    staticMesh = unreal.EditorAssetLibrary.load_asset('/Engine/BasicShapes/Cube.Cube')
+ 
+    staticMeshActor.get_component_by_class(componentClass).set_static_mesh(staticMesh)
+    # SystemLibrary.line_trace_single(start=camLocation, end=)
 
-    # staticMeshActor.get_component_by_class(componentClass).set_static_mesh(staticMesh)
-    #SystemLibrary.line_trace_single(start=camLocation, end=)
-    #location = unreal.Vector
+    DelBase = unreal.KeyEvent
+    print (DelBase)
+
+
+
+# class PyTick():
+
+#     _delegate_handle = None
+#     _current = None
+#     schedule = None
+
+#     def __init__(self):
+#         self.schedule = deque()
+#         self._delegate_handle = unreal.register_slate_post_tick_callback(self._callback)
+
+#     def _callback(self, _):
+#         if self._current is None:
+#             if self.schedule:
+#                 self._current = self.schedule.popleft()
+
+#             else:
+#                 print ('Done Jobs')
+#                 unreal.unregister_slate_post_tick_callback(self._delegate_handle)
+#                 return
+            
+#         try:
+#             task = next(self._current)
+
+#             if task is not None and isinstance(task, Iterable):
+
+#                 self.schedule.appendleft(self._current)
+#                 self._current = task
+
+#         except StopIteration:
+#             self._current = None
+
+#         except:
+#             self._current = None
+#             raise
+    
+# def my_loop():
+#     for i in range(10):
+#         print('Tick A %s'% i)
+#         yield my_inner_loop()
+
+# def my_inner_loop():
+#     for i in range(2):
+#         print ('Tick B %s'% i)
+#         yield
+
+# def checkInput():
+#     # if SplineActor.RMB == True:
+#     #     print("HELLO")
+#     #     yield checkInput()
+#     if keyboard.is_pressed('q'):
+#         print("HELLO")
+#         actorClass = unreal.StaticMeshActor
+#         componentClass = unreal.StaticMeshComponent
+#         location = unreal.Vector(0, 0, 0)
+#         staticMeshActor = EAS.spawn_actor_from_class(actorClass, location)
+#         staticMesh = unreal.EditorAssetLibrary.load_asset('/Engine/BasicShapes/Cube.Cube')
+
+#         staticMeshActor.get_component_by_class(componentClass).set_static_mesh(staticMesh)
+#         py_tick.schedule.append(checkInput())
+#     else:
+#         return
+
+# py_tick = PyTick()
+# py_tick.schedule.append(checkInput())
+
+
+###############################################
+
+# keyInfo = unreal.KeyboardInputDeviceState()
+# print (keyInfo)
+
+# def on_press(key):
+#     print('{0} pressed'.format(key))
+
+# def on_release(key):
+#     print('{0} released'.format(key))
+#     if key == Key.esc:
+#         return False
+    
+# with Listener(
+#     on_press=on_press,
+#     on_release=on_release) as listener: listener.join()
+# while True:
+#     print ("hello")
+#     break
+#     try:
+#         if keyboard.is_pressed('q'):
+#             print("HELLO")
+#             # actorClass = unreal.StaticMeshActor
+#             # componentClass = unreal.StaticMeshComponent
+#             # location = unreal.Vector(0, 0, 0)
+#             # staticMeshActor = EAS.spawn_actor_from_class(actorClass, location)
+#             # staticMesh = unreal.EditorAssetLibrary.load_asset('/Engine/BasicShapes/Cube.Cube')
+        
+#             # staticMeshActor.get_component_by_class(componentClass).set_static_mesh(staticMesh)
+#     except:
+#         break
+
+
+
